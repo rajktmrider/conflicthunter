4c55f53ef4be44dac9d1c7d655674574118f8d3c
rajktmrider <mrajcivil96@gmail.com>
2021-04-09 14:10:49 +0530

    legend

---
+++ src/test/java/org/sample/conflict/ConflictHunt.java
@@ -0,0 +1,14 @@
+package org.sample.conflict;
+
+public class ConflictHunt {
+	private void sample1() {
+		 System.out.println("sample1");
+		
+	}
+	 private void sample2() {
+		System.out.println("sample2");
+	}
+	 private void sample3() {
+		System.out.println("sample3");
+	}
+}
